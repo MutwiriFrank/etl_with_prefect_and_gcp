90a991dbe3275652239b26872294ba0a6f407191
MutwiriFrank <mutwirifranco@gmail.com>
2023-08-21 07:00:24 +0300

    write to bigquerry

--- flows/etl_gcs_to_bq.py
+++ flows/etl_gcs_to_bq.py
@@ -6,6 +6,7 @@ import pyarrow as pa
 from pyarrow.parquet import ParquetFile
 from prefect.tasks import task_input_hash
 from datetime import timedelta
+from prefect_gcp import GcpCredentials
 
 
 @task(retries=2)
@@ -16,6 +17,8 @@ def extract_from_gcp(color:str, year:int, month:int) -> Path:
     gcs_block.get_directory(from_path=gcs_path, local_path=f"../data/")
     return Path(f"../data/{gcs_path}")
 
+
+@task(retries=2)
 def transform_data(path:Path) -> pd.DataFrame:
     df = pd.read_parquet(path)
     print(f"Pre: Misssing passenger count: {df['passenger_count'].isna().sum() }")
@@ -24,11 +27,24 @@ def transform_data(path:Path) -> pd.DataFrame:
     return df
 
 
+@task(retries=2)
+def write_gcp(df:pd.DataFrame) -> None:
+    """Write to Gcp"""
+    gcp_credentials_block = GcpCredentials.load("zoom-gcp-creds")
+    df.to_gbq(
+        destination_table="taxi_data.ny_taxi",
+        project_id="data-engineering-z",
+        chunksize=200000,
+        if_exists="append",
+        credentials=gcp_credentials_block.get_credentials_from_service_account(),
+    )
+
 
 @flow()
 def gcs_to_bq(color, year, month):
     path = extract_from_gcp(color, year, month) 
     df = transform_data(path)
+    write_gcp(df)
     
 
 
